95c85b1bf834b7a3437bdfe468bed8feb5b4c654
Geoff Cordner <geoffcordner@gmail.com>
2026-01-19 10:19:37 -0800

    Create woocommerce-order-totals.sql

---
+++ woocommerce/sql/woocommerce-order-totals.sql
@@ -0,0 +1,17 @@
+/** This gives a total number of orders and the total dollar amount for each of the last 3 months ** /
+
+
+SELECT
+  DATE(paid.meta_value) AS order_day,
+  COUNT(*) AS completed_orders,
+  ROUND(SUM(total.meta_value + 0), 2) AS gross_sales
+FROM wp_posts o
+JOIN wp_postmeta paid
+  ON paid.post_id = o.ID AND paid.meta_key = '_paid_date'
+JOIN wp_postmeta total
+  ON total.post_id = o.ID AND total.meta_key = '_order_total'
+WHERE o.post_type = 'shop_order'
+  AND o.post_status = 'wc-completed'
+  AND paid.meta_value >= (NOW() - INTERVAL 3 MONTH)
+GROUP BY order_day
+ORDER BY order_day;
