2df7bf570dfc27d831e2536f461c0367063ded01
Geoff Cordner <geoffcordner@gmail.com>
2026-02-10 11:35:03 -0800

    Update woocommerce-order-totals.sql

--- woocommerce/sql/woocommerce-order-totals.sql
+++ woocommerce/sql/woocommerce-order-totals.sql
@@ -1,4 +1,4 @@
-/** This gives a total number of orders and the total dollar amount for each of the last 3 months ** /
+/* This gives a total number of orders and the total dollar amount for each of the last 3 months */
 
 
 SELECT
@@ -15,3 +15,20 @@ WHERE o.post_type = 'shop_order'
   AND paid.meta_value >= (NOW() - INTERVAL 3 MONTH)
 GROUP BY order_day
 ORDER BY order_day;
+
+/* By week */
+
+SELECT
+  DATE_SUB(DATE(paid.meta_value), INTERVAL WEEKDAY(paid.meta_value) DAY) AS week_start,
+  COUNT(*) AS completed_orders,
+  ROUND(SUM(total.meta_value + 0), 2) AS gross_sales
+FROM wp_posts o
+JOIN wp_postmeta paid
+  ON paid.post_id = o.ID AND paid.meta_key = '_paid_date'
+JOIN wp_postmeta total
+  ON total.post_id = o.ID AND total.meta_key = '_order_total'
+WHERE o.post_type = 'shop_order'
+  AND o.post_status = 'wc-completed'
+  AND paid.meta_value >= (NOW() - INTERVAL 3 MONTH)
+GROUP BY week_start
+ORDER BY week_start;
